5f287ba83dcc12e9110be8fdf99339b0dd6e303b
FANFAN <francoisnoel62@gmail.com>
2021-09-06 12:30:53 +0200

    contacts import-part II

--- tools/views.py
+++ tools/views.py
@@ -1,19 +1,44 @@
+import csv
+
 from django.shortcuts import render, redirect
+
+from contacts.models import Contact
 from .forms import ImportContactsModelForm
+from .models import ContactsImport
+
+
+def process_data(path_to_csv=False):
+    if path_to_csv:
+        with open(path_to_csv) as f:
+            reader = csv.reader(f, delimiter=";")
+            next(reader, None)
+            for row in reader:
+                _, created = Contact.objects.get_or_create(
+                    is_active=row[0],
+                    title=row[1],
+                    lang=row[2],
+                    lastname=row[3],
+                    name=row[4],
+                    age=row[5],
+                    street=row[6],
+                    region_zip=row[7],
+                    city=row[8],
+                    country=row[9],
+                    phone=row[10],
+                    mobile=row[11],
+                    email=row[12],
+                    state=row[13],
+                )
 
 
 def upload_file(request):
     if request.method == 'POST':
         form = ImportContactsModelForm(request.POST, request.FILES)
-        print(f"The form is : {form}")
         if form.is_valid():
-            print("is valid")
-            # file is saved
-            form.save()
+            file_id = form.save()
+            doc = ContactsImport.objects.get(pk=file_id.id)
+            process_data(path_to_csv=doc.file.path)
             return redirect("contacts")
-        else:
-            print("the form is not valid")
     else:
-        print("not POST")
         form = ImportContactsModelForm()
     return render(request, 'tools/contacts_import.html', {'form': form})
