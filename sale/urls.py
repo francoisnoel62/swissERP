24a4799313d88cc79741da197ad812977e57bf63
FANFAN <francoisnoel62@gmail.com>
2021-09-12 12:38:04 +0200

    add products on contact

--- sale/urls.py
+++ sale/urls.py
@@ -8,6 +8,6 @@ urlpatterns = [
     path('<int:pk>/edit', views.SaleOrderUpdateViewWithSOL.as_view(), name='edit_sale'),
     path('<int:pk>/', views.SaleOrderDetailView.as_view(), name='sale_detail'),
     path('<int:pk>/delete', views.SaleOrderDeleteView.as_view(), name='delete_sale'),
-    path('<int:order_id>/confirm', views.confirm_order, name='confirm_sale')
+    path('<int:order_id>/confirm', views.confirm_order, name='confirm_sale'),
 
 ]
