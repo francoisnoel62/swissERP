17cfc953c54a3aa5fe9a31f7e215e0f3ef506954
francoisnoel62 <francoisnoel62@gmail.com>
2022-09-09 23:05:47 +0200

    check is user is authenticated when reset password

--- accounts/urls.py
+++ accounts/urls.py
@@ -4,13 +4,4 @@ from .views import SignUpView
 
 urlpatterns = [
     path('register/', SignUpView.as_view(), name='register'),
-    # path('<int:pk>/edit/', views.EditUser.as_view(), name='edit_user'),
-    # path('change_password/', views.change_pass, name='change_pass'),
-    # path('reset_password/', views.reset_password, name='reset_password'),
-    # path('password-reset-confirm/<uidb64>/<token>',
-    #      auth_view.PasswordResetConfirmView.as_view(),
-    #      name="password_reset_confirm"),
-    # path('password_reset_complete/',
-    #      views.PasswordResetCompleteViewInherit.as_view(),
-    #      name="password_reset_complete"),
 ]
