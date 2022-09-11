0b91cbfdd1a49a995edd623748e95e2d47b182e4
francoisnoel62 <francoisnoel62@gmail.com>
2022-09-11 18:49:19 +0200

    add tests for register page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.views import LoginView
+from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
 from django.views import generic
 
@@ -8,6 +9,11 @@ class SignUpView(generic.CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+    def get(self, request, *args, **kwargs):
+        if self.request.user.is_authenticated:
+            return redirect("home")
+        return super().get(request, *args, **kwargs)
 
 
 class CustomLoginView(LoginView):
---
+++ tests/tests_register_page.py
@@ -0,0 +1,38 @@
+from django.contrib.auth.models import User
+from django.test import TestCase
+from django.urls import reverse
+
+from faker import Faker
+
+faker = Faker()
+
+
+class RegisterPageTest(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(
+            username=faker.name(),
+            email=faker.email(),
+            password=faker.password()
+        )
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/accounts/register/")
+        self.assertEqual(response.status_code, 200)
+
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("register"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("register"))
+        self.assertTemplateUsed(response, "registration/signup.html")
+
+    def test_template_content(self):
+        response = self.client.get(reverse("register"))
+        self.assertContains(response, "<p class=\"h4 mb-4\">Sign up</p>")
+
+    def test_already_user_redirection(self):
+        self.client.force_login(user=self.user)
+        response = self.client.get(reverse("register"))
+        self.assertRedirects(response, reverse("home"), status_code=302, target_status_code=200)
