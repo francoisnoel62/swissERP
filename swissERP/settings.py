edd1c52ca35b4b40d1b17dd584286d33bdfcb33b
francoisnoel62 <francoisnoel62@gmail.com>
2022-09-19 13:39:55 +0200

    ass mailgun as email services for password reset

--- swissERP/settings.py
+++ swissERP/settings.py
@@ -206,9 +206,9 @@ if IS_HEROKU:
     # Sendgrid
     EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
     DEFAULT_FROM_EMAIL = os.environ["DEFAULT_FROM_EMAIL"]
-    EMAIL_HOST = "smtp.sendgrid.net"
-    EMAIL_HOST_USER = "apikey"
-    EMAIL_HOST_PASSWORD = os.environ["EMAIL_HOST_PASSWORD"]
+    EMAIL_HOST = os.environ["MAILGUN_SMTP_SERVER"]
+    EMAIL_HOST_USER = os.environ["MAILGUN_SMTP_LOGIN"]
+    EMAIL_HOST_PASSWORD = os.environ["MAILGUN_SMTP_PASSWORD"]
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
 else:
