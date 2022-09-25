585023093cb0de9025f999c482871ee68a878d94
francoisnoel62 <francoisnoel62@gmail.com>
2022-09-25 21:13:21 +0200

    new app : sessions2

---
+++ apps/sessions/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class SessionsConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'apps.sessions'
+    label = 'classe_sessions'
