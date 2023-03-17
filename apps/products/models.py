e97212114c9ddc382975a195699f8eb8de2e3f6e
francoisnoel62 <francoisnoel62@gmail.com>
2023-03-17 16:43:29 +0100

    classes remaining

--- apps/products/models.py
+++ apps/products/models.py
@@ -11,12 +11,15 @@ class Product(models.Model):
     name = models.CharField(verbose_name="Nom du produit", max_length=50)
     price = models.FloatField(verbose_name="Prix du produit")
     description = models.TextField(verbose_name="Description du produit", null=True)
-    picture = models.ImageField(verbose_name="Illustrations", upload_to='images', null=True, blank=True)
     created_by = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE)
+    is_subscription = models.BooleanField(verbose_name="Abonnement", default=False)
+    classes_remaining = models.IntegerField(verbose_name="Nombre de cours restants", default=10)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
         return reverse('products')
+
+
 
