8fef9c2041308cfb5b2d819a18eb168850e8c16d
francoisnoel62 <francoisnoel62@gmail.com>
2023-04-20 18:18:39 +0200

    add CustomeUser tests for coverage

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,25 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from faker import Faker
+
+from accounts.models import CustomUser
+
+faker = Faker()
+
+
+class CustomUserTests(TestCase):
+    def setUp(self):
+        self.user = CustomUser.objects.create_user(
+            username=faker.user_name(),
+            email=faker.email(),
+            password=faker.password(),
+            street=faker.street_address(),
+            zip=faker.postcode(),
+            city=faker.city(),
+            telephone=faker.phone_number()
+        )
+
+    def test_get_absolute_url(self):
+        url = reverse('home')
+        self.assertEqual(self.user.get_absolute_url(), url)
