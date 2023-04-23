e377f85864d19a0a6b622036c9e948f7d06ceee1
francoisnoel62 <francoisnoel62@gmail.com>
2023-04-23 17:24:05 +0200

    update settings.py - partIII

--- swissERP/settings.py
+++ swissERP/settings.py
@@ -111,7 +111,7 @@ DATABASES = {
 
 if "DATABASE_URL" in os.environ:
     # Configure Django for DATABASE_URL environment variable.
-    DATABASES["default"] = dj_database_url.config(conn_max_age=MAX_CONN_AGE, ssl_require=True)
+    DATABASES["default"] = dj_database_url.config(ssl_require=True)
 
     # Enable test database if found in CI environment.
     if "CI" in os.environ:
